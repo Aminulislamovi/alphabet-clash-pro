aada93618dc3143e63fdecf8ba92cdca2039f27f
Aminulislamovi <md.aminul4480@gmail.com>
2024-04-04 10:00:10 +0600

    added button and score set

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,33 @@
+// function play (){
+// //    step 1 hide the home screen to hide the screen add the class hidden to the home section
+// const homeSection =document.getElementById('home-screen')
+// homeSection.classList.add('hidden');
+// // console.log(homeSection.classList)
+
+// // show the palyGround 
+//  const playGroundSection =document.getElementById('play-ground ')
+//  playGroundSection.classList.remove('hidden')
+// //  console.log(playGroundSection.classList)
+
+// }
+function continueGame(){
+//   step 1 random alphabet genarate
+
+const alphabet = getRandomAlphabet();
+
+console.log ( ' your random alphabet', alphabet);
+
+// set random alphabet generated alphabet to the screen show it 
+
+const currentAlphabetElement = document.getElementById ('current-alphabet') ;
+currentAlphabetElement.innerText =alphabet;
+
+}
+
+function play (){
+    hideElementById('home-screen')
+
+    showElementById('play-ground');
+
+    continueGame ();
+}
---
+++ scripts/utility.js
@@ -0,0 +1,38 @@
+function hideElementById (elementId){
+    const element =document.getElementById(elementId)
+    element.classList.add('hidden')
+}
+function showElementById (elementId){
+    const element = document.getElementById(elementId);
+element.classList.remove('hidden');
+}
+
+function getRandomAlphabet (){
+    // get or create an alphabet Array
+
+    const alphabetString ='abcdefghijklmnopqrstuvwxyz';
+  const alphabets =alphabetString.split(' ');
+     console.log(alphabets);
+
+    //  get a random  index between 0-25
+    //  get a random 
+
+
+    const randomNumber = Math.random() * 25;
+    const index =Math.round(randomNumber)
+  
+    const alphabet = alphabets[index];
+    console.log(index,);
+    return alphabet;
+}
+
+// function getARandomAlphabet(){
+//      const alphabetString = 'abcdefghijklmnopqrstuvwxyz' ;
+//      const alphabets =alphabetString.split(' ');
+
+//      const randomNumber = Math.random()*25;
+//      const index = Math.round(randomNumber);
+
+//      const alphabet = alphabets[index];
+//       return alphabet;
+// }
